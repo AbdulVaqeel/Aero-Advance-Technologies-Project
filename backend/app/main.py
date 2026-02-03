3df4ca649a7300e462a1056c582c7c1c3b9e9500
AbdulVaqeel <abdulvaqeel9636@gmail.com>
2026-02-03 13:19:46 +0300

    allow origins for domain in production

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,9 +7,11 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
